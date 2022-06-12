a05c34a3d08a1f1996c657c7ebb91a3806e815d1
mec-nyan <marianoraspberrypi@gmail.com>
2022-06-12 20:14:23 -0300

    Using the mouse and keypresses

--- src/index.js
+++ src/index.js
@@ -1,23 +1,38 @@
 import "./style.css";
 
+const body = document.body;
 const btn = document.getElementById("btn1");
 
 const audioContext = new AudioContext();
 
 const osc = audioContext.createOscillator();
+
 osc.type = "sawtooth";
 osc.frequency.value = 55;
-osc.start();
 
-function play() {
-  if (audioContext.state === "suspended") audioContext.resume();
-  osc.connect(audioContext.destination);
+const gain = audioContext.createGain();
+
+osc.connect(gain);
+gain.connect(audioContext.destination);
+osc.start();
+gain.gain.value = 0;
+
+function play(e) {
+  if (e.type === "mousedown" || (e.type === "keydown" && e.key === "j")) {
+    gain.gain.value = 1;
+    btn.classList.add("hightlighted");
+  }
 }
 
-function stop() {
-  if (audioContext.state === "suspended") audioContext.resume();
-  osc.disconnect();
+function stop(e) {
+  if (e.type === "mouseup" || (e.type === "keyup" && e.key === "j")) {
+    gain.gain.value = 0;
+    btn.classList.remove("hightlighted");
+  }
 }
 
 btn.addEventListener("mousedown", play);
 btn.addEventListener("mouseup", stop);
+
+body.addEventListener("keydown", play);
+body.addEventListener("keyup", stop);
